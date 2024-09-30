1b145d198cfb887fcf4daa51a375f5276c7ac04e
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 16:23:58 +0200

    Auth routes

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf import settings
+from account.views import *
+from django.conf.urls.static import static
+
+app_name = 'auth'
+
+urlpatterns = [
+    path('login/', user_login, name="login"),
+    path('register/', user_register, name="register"),
+    path('logout/', user_logout, name='logout'),
+
+    path('profile/', userProfile, name='userProfile'),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
