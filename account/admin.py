7221cd5df92cbf60d44b2a6ffaabf2a07e7e0848
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 19:16:51 +0200

    New changes

--- account/admin.py
+++ account/admin.py
@@ -3,21 +3,8 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from account.models import *
 
-class UserAdmin(BaseUserAdmin):
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('name', 'phone_number', 'image')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Role'), {'fields': ('role',)}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'name', 'phone_number', 'role', 'password1', 'password2'),
-        }),
-    )
-    list_display = ('email', 'name', 'phone_number', 'role', 'is_staff', 'added_by')
-    search_fields = ('email', 'name', 'phone_number', 'role')
-    ordering = ('email',)
-
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'role', 'added_by', 'is_active', 'is_staff')
+    search_fields = ('name', 'email', 'phone_number')
+    prepopulated_fields = {'slug': ('name',)}
--- account/models.py
+++ account/models.py
@@ -1,8 +1,8 @@
 import os
 from django.db import models
+from account.managers import *
 from django.utils import timezone
 from django.utils.text import slugify
-from account.managers import CustomUserManager
 from imagekit.processors import ResizeToFill
 from imagekit.models import ProcessedImageField
 from django.utils.translation import gettext_lazy as _
@@ -10,7 +10,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 def user_image_path(instance, filename):
     base_filename, file_extension = os.path.splitext(filename)
-    return f'profile_images/user_{slugify(instance.name)}_{instance.phone_number}{file_extension}'
+    return f'profile_images/user_{slugify(instance.name)}_{instance.pk}{file_extension}'
 
 class User(AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = (
@@ -19,6 +19,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
     phone_number = models.CharField(max_length=15, unique=True)
     image = ProcessedImageField(
         upload_to=user_image_path,
@@ -29,27 +30,29 @@ class User(AbstractBaseUser, PermissionsMixin):
         blank=True,
     )
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
-    status = models.BooleanField(default=False)
 
     added_by = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='added_users')
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'phone_number']
 
+    def save(self, *args, **kwargs):
+        if self.pk:
+            existing_user = User.objects.get(pk=self.pk)
+            if not self.slug or self.name != existing_user.name:
+                self.slug = slugify(self.name)
+        else:
+            if not self.slug:
+                self.slug = slugify(self.name)
+                
+        super(User, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.email
-
-    def save(self, *args, **kwargs):
-        # Set status based on role selection
-        if self.role == 'Trainee':
-            self.status = True
-        elif self.role == 'Trainer':
-            self.status = False
