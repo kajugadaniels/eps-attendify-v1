15bd1a40ef09ff8283f3c490feb7d7f8a049af73
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 19:53:25 +0200

    Add getEmployee function

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from home.forms import *
+from home.models import *
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
 @login_required
 def dashboard(request):
@@ -9,3 +12,13 @@ def dashboard(request):
     }
 
     return render(request, 'dashboard.html', context)
+
+@login_required
+def getEmployees(request):
+    employees = Employee.objects.all().order_by('-date_of_hire')
+
+    context = {
+        'employees': employees
+    }
+
+    return render(request, 'employees/index.html', context)
