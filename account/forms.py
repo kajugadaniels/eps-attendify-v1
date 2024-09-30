159dd9af41cea40576a655f5fa3aebe78f8ba33c
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 16:26:20 +0200

    Add register form

--- account/forms.py
+++ account/forms.py
@@ -22,3 +22,33 @@ class LoginForm(forms.Form):
             if user is None:
                 raise forms.ValidationError("Invalid email or password")
         return cleaned_data
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'required': 'true', 'placeholder': 'Password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'required': 'true', 'placeholder': 'Confirm Password'}), label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'phone_number', 'role', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'required': 'true', 'placeholder': 'Full Name'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'required': 'true', 'placeholder': 'Email Address'})
+        self.fields['phone_number'].widget.attrs.update({'class': 'form-control', 'required': 'true', 'placeholder': 'Phone Number'})
+        self.fields['role'].widget.attrs.update({'class': 'form-control', 'required': 'true'})
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
