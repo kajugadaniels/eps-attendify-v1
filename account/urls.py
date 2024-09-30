675f98a3076c7f69dac46a1ab33aec6def858de5
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 16:50:46 +0200

    New changes

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'auth'
 
 urlpatterns = [
-    path('login/', user_login, name="login"),
+    path('', user_login, name="login"),
     path('register/', user_register, name="register"),
     path('logout/', user_logout, name='logout'),
 
--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 app_name = 'base'
 
 urlpatterns = [
-    path('', dashboard, name="dashboard")
+    path('dashboard/', dashboard, name="dashboard")
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
