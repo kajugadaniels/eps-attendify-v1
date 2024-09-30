feea5429b4b0ce14f1d680dd2c6087f3c69037c0
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 16:27:31 +0200

    Add login function

--- account/views.py
+++ account/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import logging
+from account.forms import *
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login, logout, update_session_auth_hash
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('base:dashboard')
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    auth_login(request, user)
+                    messages.success(request, _('Login successful!'))
+                    return redirect('base:dashboard')
+                else:
+                    messages.error(request, _('Your account is inactive.'))
+            else:
+                messages.error(request, _('Invalid email or password'))
+        else:
+            messages.error(request, _("Please correct the error below."))
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'auth/login.html', context)
