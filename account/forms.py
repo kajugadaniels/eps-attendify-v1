1b085336b38272fa9b421db89d1e58bc45cb76c7
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 16:25:59 +0200

    Add login form

---
+++ account/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from account.models import *
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import PasswordChangeForm
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        max_length=255,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '**********'})
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            user = authenticate(email=email, password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid email or password")
+        return cleaned_data
