20103d44495a7dc002f8b0a67cfd3c75efb46a4f
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-30 19:42:38 +0200

    Register models in admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
+from home.models import *
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'national_id', 'email', 'department', 'position', 'date_of_hire', 'tag_id', 'phone_number', 'address']
+    search_fields = ['name', 'national_id', 'email']
+    list_filter = ['department', 'position', 'date_of_hire']
