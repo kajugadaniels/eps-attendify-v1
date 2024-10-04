f6fd5f22cb3760051be15fda4c75bc593848bd83
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-04 13:27:05 +0200

    register employee attendence

--- home/admin.py
+++ home/admin.py
@@ -11,3 +11,9 @@ class EmployeeAdmin(admin.ModelAdmin):
     list_display = ['name', 'national_id', 'email', 'department', 'position', 'date_of_hire', 'tag_id', 'phone_number', 'address', 'day_salary']
     search_fields = ['name', 'national_id', 'email']
     list_filter = ['department', 'position', 'date_of_hire']
+
+@admin.register(EmployeeAttendance)
+class EmployeeAttendanceAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'attended', 'created_at')
+    list_filter = ('attended', 'created_at', 'employee')
+    search_fields = ('employee__name', 'employee__national_id', 'employee__tag_id')
