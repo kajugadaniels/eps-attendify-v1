eb88039f71dc1ed25fe07de57adeca37f3e9f218
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-11 20:02:40 +0200

    Add more fields in employee table

--- home/models.py
+++ home/models.py
@@ -17,6 +17,15 @@ class Employee(models.Model):
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     day_salary = models.CharField(max_length=40, null=True, blank=True)
+    week_ending = models.CharField(max_length=40, null=True, blank=True)
+    budget_ref = models.CharField(max_length=40, null=True, blank=True)
+    supervision = models.CharField(max_length=40, null=True, blank=True)
+    field = models.CharField(max_length=40, null=True, blank=True)
+    type = models.CharField(max_length=40, null=True, blank=True)
+    nid = models.CharField(max_length=40, null=True, blank=True)
+    rssb_number = models.CharField(max_length=40, null=True, blank=True)
+    rate = models.CharField(max_length=40, null=True, blank=True)
+    gross = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} - {self.position.name if self.position else 'No Position'}"
